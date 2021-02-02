171585662fa6b526be58e54bbb6c8cebca1a7d45
Hat_Cloud <mr.hatcloud@gmail.com>
2021-02-02 12:37:40 +0800

    Initial Commit

---
+++ Shared/ReaderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderDemoApp.swift
+//  Shared
+//
+//  Created by Hat_Cloud on 21/2/2.
+//
+
+import SwiftUI
+
+@main
+struct ReaderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
